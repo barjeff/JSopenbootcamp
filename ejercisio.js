8ba3037510dcae8bbe742ef95c54ce44b5491cac
jefferson magallanes <jeffersonmaga97@gmail.com>
2022-09-06 14:51:51 -0300

    primerEjercicio

---
+++ ejercisio.js
@@ -0,0 +1,14 @@
+const infofecha = new Date("february 03 1997")
+
+let nombre =  jefferson;
+let edad = 25;
+let desarrollador = true;
+let fechaNacimiento = infofecha;
+
+
+const libro = {
+ titulo: "harry potter",
+ autor: "j k R",
+ fecha : "2022",
+ URL: "https://harrypottershop.com/"
+}
